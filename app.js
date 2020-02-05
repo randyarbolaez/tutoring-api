a4a244ac48ecdf45043bbc331ad3856d00d9bf50
Randy <randyarbolaez0@gmail.com>
2020-02-05 18:58:51 -0500

    make it better

---
+++ app.js
@@ -0,0 +1,117 @@
+require("dotenv").config({ path: ".env" });
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const app = express();
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const User = require("./models/user-schema");
+
+// DB Setup
+mongoose.Promise = Promise;
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
+// DB Setup
+
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
+
+// Middleware Setup
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "our-passport-local-strategy-app",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.set("views", path.join(__dirname, "views"));
+// app.use(express.static(path.join(__dirname, "/public")));
+// app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
+// PASSPORT CONFIG
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+
+passport.use(
+  new LocalStrategy(
+    {
+      passReqToCallback: true
+    },
+    (req, username, password, next) => {
+      User.findOne(
+        {
+          username
+        },
+        (err, user) => {
+          if (err) {
+            return next(err);
+          }
+          if (!user) {
+            return next(null, false, {
+              message: "Incorrect username"
+            });
+          }
+          if (!bcrypt.compareSync(password, user.password)) {
+            return next(null, false, {
+              message: "Incorrect password"
+            });
+          }
+
+          return next(null, user);
+        }
+      );
+    }
+  )
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Route
+const authRoutes = require("./routes/auth-routes");
+app.use("/", authRoutes);
+//Route
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Listening on http://localhost:${process.env.PORT}`);
+});
+
+module.exports = app;
---
+++ routes/auth-routes.js
@@ -0,0 +1,58 @@
+require("dotenv").config({ path: "../.env" });
+
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user-schema");
+
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+
+router.post("/signup", (req, res, next) => {
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  new User({
+    username: req.body.username,
+    password: bcrypt.hashSync(req.body.password, salt)
+  }).save((err, doc) => {
+    if (!err) {
+      console.log(doc, "doccc");
+      res.send(doc);
+    } else {
+      if (err.code == 11000) {
+        res
+          .status(422)
+          .send(["Username was already found, use a different username!"]);
+      }
+    }
+  });
+});
+
+router.post("/authenticate", (req, res, next) => {
+  function generateJwt() {
+    return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+      expiresIn: "10m"
+    });
+  }
+
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      //err passport middleware
+      return res.status(400).json(err);
+    } else if (user) {
+      //registered user
+      return res.status(200).json({ token: generateJwt() });
+    } else {
+      // unknown user/wrong password
+      return res.status(404).json(info);
+    }
+  })(req, res);
+});
+
+router.post("/logout", (req, res, next) => {
+  req.logout();
+  res.send(doc);
+});
+
+module.exports = router;
